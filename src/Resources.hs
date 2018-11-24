a193829eb92f2f2a66debf52568a6ce12429f74b
MarkWRuszczycky <mwruszczycky@gmail.com>
2018-11-24 10:19:00 -0600

    Fixes typo in BSD2 license reproduction

--- src/Resources.hs
+++ src/Resources.hs
@@ -444,7 +444,7 @@ bsd2License = unlines
   , "Redistribution and use in source and binary forms, with or without"
   , "modification, are permitted provided that the following conditions"
   , "are met:\n"
-  , "    1. Redistributions of source code must retain the above copyright\n"
+  , "    1. Redistributions of source code must retain the above copyright"
   , "       notice, this list of conditions and the following disclaimer.\n"
   , "    2. Redistributions in binary form must reproduce the above"
   , "       copyright notice, this list of conditions and the following"
